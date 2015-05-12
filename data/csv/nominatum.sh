7d9a849495b9c68fad0f93c782bf8cac2b66701f
Albert Decatur <albert.decatur@gmail.com>
2015-05-12 11:01:36 -0400

    add script to geocode from OSM nominatum remove fatal encounters without address - about 70

---
+++ data/csv/nominatum.sh
@@ -0,0 +1,20 @@
+# use id and formatted address fields in TSV
+# google geocoding api output - but only take first output
+# use jq to filter JSON to just google's formatted address, lat, lng
+function geocode {
+	a="'"
+	awk -F'\t' '{OFS="\t";if($9 !~ /^$/)print $1,$9}'|\
+	parallel --gnu --colsep '\t' --header : '
+	url_encode=$( 
+	echo {2} |\
+		perl -MURI::Escape -e '$a'print uri_escape(<STDIN>); print "\n";'$a' 
+	)
+	first_return=$( 
+		curl -s "http://nominatim.openstreetmap.org/search/${url_encode}?format=json&limit=1" |\
+		jq '$a'.[]|[.display_name,.lat,.lon]|@csv'$a'
+	)
+	echo -e {1}"\t$first_return"
+	'
+}
+
+cat | geocode
